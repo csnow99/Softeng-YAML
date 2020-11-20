ead8004306f3f33296f50a8d810a48f7be55fd4c
Ivan <Ivan@192.168.2.22>
2020-11-21 00:48:34 +0100

    typo fix

--- frontEnd/js/api.js
+++ frontEnd/js/api.js
@@ -1,6 +1,6 @@
 // all access driven through BASE. Must end with a SLASH
 // be sure you change to accommodate your specific API Gateway entry point
-var base_url = "https://65y680psjl.execute-api.us-east-2.amazonaws.com/beta";
+var base_url = "https://65y680psjl.execute-api.us-east-2.amazonaws.com/beta/";
 
 var createChoice_url = base_url + "createChoice";    // POST
 var choicePage_url = base_url + "choice" //GET with {choiceID} so we avoid CORS issues
