4add53946f43ad06e11d50a9f84e6bbe6990c8f9
GTWFam <252704436go@gmail.com>
2020-11-24 12:57:23 -0500

    Now can create alternatives 3 4 and 5

--- frontEnd/js/altCreate.js
+++ frontEnd/js/altCreate.js
@@ -1,18 +1,26 @@
 function handleAltCreateClick(e){
-    var output = document.getElementById("alternatives").innerHTML;
-    var count = (output.match(/<label>/g) || []).length;
+    let alt = document.getElementById("alternatives");
+    alt.removeChild(document.getElementById("altCreate"));
+
+    let output = document.getElementById("alternatives").innerHTML;
+    let count = (output.match(/<label>/g) || []).length / 2;
     console.log(count);
-    if (count < 8){
-        let num = count / 2 + 1;
+    console.log(output);
+
+    if (count < 4){
+        let num = count + 1;
         output = output + "<label>Alternative Name: </label><input type=\"text\" name=\"altName" + num + "\" value=\"\"><br>\n" +
             "<label>Alternative Description: </label><input type=\"text\" name=\"altDesc" + num + "\" value=\"\"><br><br>\n" +
-            "<input type=\"button\" value=\"Create another alternative\" onclick=\"JavaScript:handleAltCreateClick\">";
-    } else if (count == 8) {
-        let num = count / 2 + 1;
+            "<input type=\"button\" id=\"altCreate\" value=\"Create another alternative\" " +
+            "onclick=\"JavaScript:handleAltCreateClick(this)\">";
+    } else if (count == 4) {
+        let num = count + 1;
         output = output + "<label>Alternative Name: </label><input type=\"text\" name=\"altName" + num + "\" value=\"\"><br>\n" +
             "<label>Alternative Description: </label><input type=\"text\" name=\"altDesc" + num + "\" value=\"\"><br><br>\n";
     } else {
-        output = ""
+
     }
+
+
     document.getElementById("alternatives").innerHTML = output;
 }
