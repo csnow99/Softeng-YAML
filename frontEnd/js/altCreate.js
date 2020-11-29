192fd6c256670171c7c691e10b9d2d97fdee76db
GTWFam <252704436go@gmail.com>
2020-11-29 16:48:03 -0500

    Alternatives saves the input now

--- frontEnd/js/altCreate.js
+++ frontEnd/js/altCreate.js
@@ -1,26 +1,21 @@
 function handleAltCreateClick(e){
     let alt = document.getElementById("alternatives");
-    alt.removeChild(document.getElementById("altCreate"));
-
-    let output = document.getElementById("alternatives").innerHTML;
+    let output = alt.innerHTML;
     let count = (output.match(/<label>/g) || []).length / 2;
     console.log(count);
-    console.log(output);
 
     if (count < 4){
         let num = count + 1;
-        output = output + "<label>Alternative Name: </label><input type=\"text\" name=\"altName" + num + "\" value=\"\"><br>\n" +
-            "<label>Alternative Description: </label><input type=\"text\" name=\"altDesc" + num + "\" value=\"\"><br><br>\n" +
-            "<input type=\"button\" id=\"altCreate\" value=\"Create another alternative\" " +
-            "onclick=\"JavaScript:handleAltCreateClick(this)\">";
+        let altField = "<label>Alternative Name: </label><input type=\"text\" name=\"altName" + num + "\" value=\"\"><br>\n" +
+            "<label>Alternative Description: </label><input type=\"text\" name=\"altDesc" + num + "\" value=\"\"><br><br>\n";
+        document.getElementById("alternative" + num).innerHTML = altField;
     } else if (count == 4) {
         let num = count + 1;
-        output = output + "<label>Alternative Name: </label><input type=\"text\" name=\"altName" + num + "\" value=\"\"><br>\n" +
+        let altField = "<label>Alternative Name: </label><input type=\"text\" name=\"altName" + num + "\" value=\"\"><br>\n" +
             "<label>Alternative Description: </label><input type=\"text\" name=\"altDesc" + num + "\" value=\"\"><br><br>\n";
+        document.getElementById("alternative" + num).innerHTML = altField;
+        alt.removeChild(document.getElementById("altCreate"));
     } else {
 
     }
-
-
-    document.getElementById("alternatives").innerHTML = output;
 }
