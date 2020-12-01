2d9493d0684338d498881b97ebc138b6b3534502
Ivan <Ivan@Ivan-PC>
2020-12-01 18:57:19 +0100

    Hezi - lambda starter pack

---
+++ src/main/java/edu/wpi/cs/yaml/demo/CreateReportsHandler.java
@@ -0,0 +1,12 @@
+package edu.wpi.cs.yaml.demo;
+
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import edu.wpi.cs.yaml.demo.http.ChoiceInfoList;
+
+
+public class CreateReportsHandler implements  RequestHandler<Object, ChoiceInfoList> {
+	//make DAO return a list of all choices
+	//put these in a choiceInfo list
+	//return the list
+}
