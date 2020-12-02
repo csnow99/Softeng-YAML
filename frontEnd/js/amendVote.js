dd4952006e7257885cd56673019e1db8e6ac6cd2
Charlie Snow <charles.snow@woosternet.org>
2020-12-02 14:06:20 -0500

    test button switch

--- frontEnd/js/amendVote.js
+++ frontEnd/js/amendVote.js
@@ -1,8 +1,18 @@
-function handleAmendVoteClick(number){
+function handleAmendVoteClick(id){
     console.log("Clicked!");
-    let image = document.images[number];
+    //let image = document.images[number];
+    let image = document.getElementById(id);
+    //image.src = "../img/liked.png";
     console.log(image);
-    switch (image.src){
+    if(image.alt == "like"){
+        image.src = "../img/liked.png"
+        image.alt = "liked"
+    }
+    else if(image.alt == "liked"){
+        image.src = "../img/like.png"
+        image.alt = "like"
+    }
+    /*switch (image.src){
         case "../img/like.png":
             image.src = "../img/liked.png";
             break;
@@ -15,6 +25,6 @@ function handleAmendVoteClick(number){
         case "https://yamlcs3733bucket.s3.us-east-2.amazonaws.com/img/disliked.png":
             image.src = "../img/dislike.png";
             break;
-    }
+    }*/
 
 }
