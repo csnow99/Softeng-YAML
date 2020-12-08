2541f7e274c69449ffda0386cc1919f131d520f1
Ivan <Ivan@Ivan-PC>
2020-12-08 21:53:58 +0100

    getAlternativesHandlerTest and deleteSingleChoiceByIDHandlerTest completed

--- src/test/java/edu/wpi/cs/yaml/demo/GetAlternativesHandlerTest.java
+++ src/test/java/edu/wpi/cs/yaml/demo/GetAlternativesHandlerTest.java
@@ -1,6 +1,5 @@
 package edu.wpi.cs.yaml.demo;
 
-import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,11 +7,9 @@ import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
 
-
+import edu.wpi.cs.yaml.demo.db.ChoiceDAO;
 import edu.wpi.cs.yaml.demo.http.CreateChoiceRequest;
 import edu.wpi.cs.yaml.demo.http.CreateChoiceResponse;
-import edu.wpi.cs.yaml.demo.http.DeleteSingleChoiceByIDRequest;
-import edu.wpi.cs.yaml.demo.http.DeleteSingleChoiceByIDResponse;
 import edu.wpi.cs.yaml.demo.http.GetAlternativesResponse;
 import edu.wpi.cs.yaml.demo.model.Alternative;
 
@@ -36,14 +33,14 @@ public class GetAlternativesHandlerTest extends LambdaTest{
 
         CreateChoiceHandler createHandler = new CreateChoiceHandler();
         CreateChoiceRequest ccr = new CreateChoiceRequest("testChoice1", 10, "sample description", alternatives);
+        
+        try {
         CreateChoiceResponse createResp = createHandler.handleRequest(ccr, createContext("create"));
         choiceID = createResp.getResponse();
       
         
         /*Now that it's inserted we may */
-        if(choiceID == null) {Assert.fail("Created ChoiceID is null");}
         GetAlternativesResponse resp = handler.handleRequest(choiceID, createContext("list"));
-        if(resp.getHttpCode() == 404) {Assert.fail("ChoiceID not found");}
         List<Alternative> respAlternatives = resp.getAlternatives();
         
         Assert.assertEquals(3, respAlternatives.size());
@@ -60,13 +57,17 @@ public class GetAlternativesHandlerTest extends LambdaTest{
         Assert.assertTrue(respAlternatives.get(2).getTitle().equals("alt3_name"));
         Assert.assertTrue(respAlternatives.get(2).getDescription().equals("alt3_description"));
         
-        
-        /*Delete the inserted choice*/
-        if (choiceID != null) {
-            DeleteSingleChoiceByIDRequest dcr = new DeleteSingleChoiceByIDRequest(choiceID);
-            DeleteSingleChoiceByIDResponse d_resp = new DeleteSingleChoiceByIDChoiceHandler().handleRequest(dcr, createContext("delete"));
-            assertEquals("Succesfully deleted: "+choiceID, d_resp.getResponse());
+        } catch(Exception e) {
+        	Assert.fail();
         }
+        /*Delete the inserted choice*/
+        try {
+         ChoiceDAO choiceDAO = new ChoiceDAO();
+         choiceDAO.deleteChoice(choiceID);
+        } catch (Exception e) {
+        	Assert.fail();
+        }
+        
     
     }
 }
